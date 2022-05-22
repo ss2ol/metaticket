1784c74550875f252c7111a325416f052c5da81a
sochong <hsju0202@gmail.com>
2022-05-23 00:29:17 +0900

    init commit

---
+++ src/test/java/com/metaticket/metaticket/MetaticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaticket.metaticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
